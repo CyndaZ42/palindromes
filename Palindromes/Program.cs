d42b6e16c1e88abbf604d4abaf0529088100c105
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-12 11:30:54 -0700

    Add Carnival worker to UI
    Co-Authored-By: Liang ZhiBin <11263320+losophy@users.noreply.github.com>
    Co-Authored-By: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 
 namespace Palindromes {
 
@@ -6,8 +7,39 @@ namespace Palindromes {
   {
     public static void Main()
     {
-      Console.WriteLine("Is your word a palindrome?");
-      Console.WriteLine("Step right up and find out!")
+      Console.WriteLine("_______________________________________________________");
+      Console.WriteLine("              Is your word a palindrome?");
+      Console.WriteLine("              Step right up and find out!");
+      Console.WriteLine("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_");
+      Carnie();
+    }
+
+    public static void Carnie()
+    {
+      Console.WriteLine("");
+      Thread.Sleep(900);
+      Console.Write("You there");
+      Thread.Sleep(100);
+      Console.Write(".");
+      Thread.Sleep(100);
+      Console.Write(".");
+      Thread.Sleep(100);
+      Console.Write(".");
+      Thread.Sleep(300);
+      Console.Write("In the blue shirt.");
+      Thread.Sleep(900);
+      Console.Write(" What is your word");
+      Console.WriteLine("");
+      string palindrome = Console.ReadLine();
+      if (Words.isPalindrome(palindrome)) {
+        Console.WriteLine("YES! We have a winner!");
+        Console.Write("The word " + palindrome + " is a palindrome");
+      } 
+      else {
+        Console.WriteLine("OHH...");
+        Thread.Sleep(600);
+        Console.Write("Not Quite...")
+      }
     }
   }
 }
